f708462a291779c64278d33c6fad011441fa683f
alexclusive <58881385+alexclusive@users.noreply.github.com>
2022-07-08 01:00:11 +1000

    Create BirthdayMemorization.cpp

---
+++ Kattis/C++/BirthdayMemorization.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    Friend** friends = new Friend*[N]; // an array of size N
+    for (int i = 0; i < N; i++) {
+        cin >> friends[i].name >> friends[i].level; // wtf c++
+    }
+}
+
+class Friend {
+    public:
+        string name;
+        int level;
+        int day;
+        int month;    
+};
