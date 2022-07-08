061c3132f20501f7096641ccb0dde81517c0d518
alexclusive <58881385+alexclusive@users.noreply.github.com>
2022-07-08 13:24:30 +1000

    Some easy Kattis

---
+++ Kattis/C++/easy/carrots/carrots.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c, p;
+    cin >> c >> p;
+    std::cout << p;
+}
---
+++ Kattis/C++/easy/r2/r2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    short r1, s;
+    cin >> r1 >> s;
+    std::cout << s + (s-r1);
+}
