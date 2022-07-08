dc5b12573dc03c9afadf4183febb493486f6152b
alexclusive <58881385+alexclusive@users.noreply.github.com>
2022-07-08 12:04:40 +1000

    Homework pass 31/43

--- Kattis/C++/easy/Homework.cpp
+++ Kattis/C++/easy/Homework.cpp
@@ -5,12 +5,17 @@ int main() {
     int a, b, sum = 0;
     char sym;
 
-    while (cin >> a >> sym) {
-        if (sym == '-') {
-            cin >> b >> sym;
-            sum += b-a + 1;
-        } else if (sym == ';') {
+    int n;
+    while (cin >> n) {
+        char ch;
+        cin >> ch;
+        if (ch == ';') {
             sum++;
+        } else if (ch == '-') {
+            int m;
+            cin >> m;
+            sum += m-n+1;
+            cin >> ch;
         }
     }
 
