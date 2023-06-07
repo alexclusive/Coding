d8c395af58acbafa48caf30582952297d50fa831
Alex <58881385+alexclusive@users.noreply.github.com>
2023-06-07 10:35:11 +1000

    work on day 9

--- AdventOfCode/2022/9/day9part2.py
+++ AdventOfCode/2022/9/day9part2.py
@@ -10,6 +10,7 @@ rope = [[0,0] for _ in range(10)]
 
 def addTail():
 	tail = (rope[-1][0], rope[-1][1])
+	tail = (rope[1][0], rope[1][1])
 	# if tail not in visited:
 	# 	print(tail)
 	visited.add(tail)
@@ -17,25 +18,16 @@ def addTail():
 def pull(head:list, tail:list) -> bool:
 	return abs(tail[0] - head[0]) > 1 or abs(tail[1] - head[1]) > 1
 
-def adjust_diagonal(head:list, tail:list, vertical) -> list:
-	if vertical:
-		pos = 0
-	else:
-		pos = 1
-
-	if head[pos] > tail[pos]:
-		tail[pos] += 1
-	elif head[pos] < tail[pos]:
-		tail[pos] -= 1
-
-	return tail
+def pull_diagonal(head:list, tail:list) -> bool:
+	return abs(tail[0] - head[0]) > 1 and abs(tail[1] - head[1]) > 1
 
 def move_knots(vertical:bool, dist:int):
 	rope[0][vertical] += dist # move head
 	for i in range(1, len(rope)):
-		if pull(rope[i-1], rope[i]):
-			rope[i] = adjust_diagonal(rope[i-1], rope[i], vertical)
-			rope[i][1] += dist
+		if pull_diagonal(rope[i-1], rope[i]):
+			rope[i][vertical] += dist
+		# if i == 1:
+		# 	print(rope[0], rope[i])
 
 def step_through_knots(steps, vertical, positive):
 	dist = positive*2 - 1 # True -> 1, False -> -1
@@ -53,26 +45,6 @@ for line in contents:
 	vertical = direction == "U" or direction == "D"
 	positive = direction == "U" or direction == "R"
 	step_through_knots(steps, vertical, positive)
-	
-	# if direction == "U":
-	# 	for step in range(steps):
-	# 		move_knots(True, 1)
-	# 		addTail()
-
-	# elif direction == "D":
-	# 	for step in range(steps):
-	# 		move_knots(True, -1)
-	# 		addTail()
-
-	# elif direction == "L":
-	# 	for step in range(steps):
-	# 		move_knots(False, -1)
-	# 		addTail()
-
-	# elif direction == "R":
-	# 	for step in range(steps):
-	# 		move_knots(False, 1)
-	# 		addTail()
 
 print(visited)
 print(len(visited))
