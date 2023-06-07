3f09c27b87a6056b0a3449c8eccc7fe2f4067cee
Alex <58881385+alexclusive@users.noreply.github.com>
2023-06-07 12:07:02 +1000

    work on day 9

--- AdventOfCode/2022/9/day9part2.py
+++ AdventOfCode/2022/9/day9part2.py
@@ -9,7 +9,7 @@ visited:set = set()
 rope = [[0,0] for _ in range(10)]
 
 def addTail():
-	tail = (rope[-1][0], rope[-1][1])
+	# tail = (rope[-1][0], rope[-1][1])
 	tail = (rope[1][0], rope[1][1])
 	# if tail not in visited:
 	# 	print(tail)
@@ -24,10 +24,13 @@ def pull_diagonal(head:list, tail:list) -> bool:
 def move_knots(vertical:bool, dist:int):
 	rope[0][vertical] += dist # move head
 	for i in range(1, len(rope)):
-		if pull_diagonal(rope[i-1], rope[i]):
-			rope[i][vertical] += dist
-		# if i == 1:
-		# 	print(rope[0], rope[i])
+		lr = abs(rope[i][0] - rope[i-1][0]) # left right
+		tb = abs(rope[i][1] - rope[i-1][1]) # top bottom
+
+		if lr > 1:
+			rope[i][0] += dist
+		if tb > 1:
+			rope[i][1] += dist
 
 def step_through_knots(steps, vertical, positive):
 	dist = positive*2 - 1 # True -> 1, False -> -1
